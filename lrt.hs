931e935cdc7e317f4517b5c39831f86b25d02e36
jcc333 <clemer19@gmail.com>
2015-02-28 14:54:15 -0600

    Create propper ordering over trees

--- lrt.hs
+++ lrt.hs
@@ -1,7 +1,9 @@
 module Lrt where
 import Data.Monoid as Monoid
 import Data.Maybe as Maybe
-import Data.Map as Map
+import qualified Data.Map as Map
+
+type Map k v = Map.Map k v
 
 data Node a = Root -- used in the top level of the tree
             | Vertex a deriving (Eq, Show)
@@ -10,50 +12,66 @@ instance Functor Node where
   fmap f (Vertex a) = Vertex $ f a
   fmap f Root = Root
 
---instance (Monoid a) => Monoid Node a where
-data Lrt a = Inc (Node a) (Map a Lrt a)
+data Lrt a = Inc (Node a) (Map a (Lrt a))
            | Exc (Node a) (Lrt a)
            | Nil
            deriving (Eq, Show)
+
+node (Inc n _) = Just n
+node (Exc n _) = Just n
+node Nil = Nothing
 
 vertex (Inc (Vertex a) _) = Just a
 vertex (Exc (Vertex a) _) = Just a
 vertex _ = Nothing
 
 children (Inc _ bs) = bs
-children (Exc _ b) = Map.singleton $ vertex b b
+children (Exc _ b) = case vertex b of
+  Just v -> Map.singleton v b
+  Nothing -> Map.empty
 children _ = Map.empty
 
-subgraph (Inc la lbs) (Inc ra rbs) = 
-  la == ra &&
-    all (\ l -> elem l rightVertices) leftVertices &&
-      all (subgraphAny rbs) lbs
-        where 
-          leftVertices = catMaybes (map vertex lbs)
-          rightVertices = catMaybes (map vertex rbs)
-          subgraphAny ts t = any (\ r -> t `subgraph` r) ts
-subgraph (Exc la lb) (Exc ra rb) = la == ra && lb `subgraph` rb
-subgraph (Exc la lb) (Inc ra rbs) = la == ra && lb `elem` rbs
-subgraph (Inc _ _) (Exc _ _) = False
+-- We define a partial ordering by specificity for propositional trees:
+-- we say for two trees a and b, a <= b iff:
+--    b is a subgraph of a
+--    labels in a are at least as restrictive as in b
+-- examples:
+--   YES: 0 . [1 2 3] <= 0 . [1 2] : there's a branch in b for each branch in a
+--   YES: 0 ! 1 <= 0 . [1 2] : the above, plus, a is more specific
+--   NO:  0 . 1 <= 0 ! 1 : b is more restrictive than a
+--   NO:  0 . [1 2] <= 0 . [1 2 3 4 5] : b is more specified than a
+--   YES: F <= 0 ! 1 : F is the least LRT
+--   YES: 0 . [(some large set of propositions)] <= T : T is greatest LRT
+instance (Ord a) => Ord (Lrt a) where
+  (<=) Nil _ = True
+  (<=) (Inc la lbs) (Inc ra rbs) =
+    la == ra && all (\ (_, t) -> isJust $ t `subgraphLookup` lbs) (Map.toList rbs)
+  (<=) (Exc la lb) (Exc ra rb) = la == ra && rb <= lb
+  (<=) (Exc la lb) (Inc ra rbs) = la == ra && (isJust $ lb `subgraphLookup` rbs)
+  (<=) (Inc _ _) (Exc _ _) = False
+
+subgraphLookup t bs = 
+  vertex t >>= \ v ->
+    Map.lookup v bs >>= \ b ->
+      if t <= b
+      then Just b
+      else Nothing
 
 greatestLower :: Lrt a -> Lrt a -> Lrt a
-greatestLower lhs rhs = 
-  case (lhs, rhs) of
+greatestLower lhs rhs = lhs
+  {-case (lhs, rhs) of
     (Inc a lbs,  Inc a rbs) -> Inc a lbs ++ rbs
-    (Inc la lbs, Inc ra rbs) ->
+    (Inc la lbs, Inc ra rbs) -> 
     (Exc la lb,  Inc ra rbs) -> 
     (Inc la lbs, Exc ra rb)  -> 
     (Exc la lb,  Exc ra rbs  -> 
   where lesserEdge (Inc a [b]) (Exc a b) = Just $ Exc a b
         lesserEdge (Exc a b) (Inc a [b]) = Just $ Exc a b
         lesserEdge (Exc a b) (Exc a b) = Just $ Exc a b
-        lesserEdge _ _ = Nothing
+        lesserEdge _ _ = Nothing-}
 
 leastUpper :: Lrt a -> Lrt a -> Lrt a
 leastUpper lhs rhs = lhs
-
-instance Eq a => Ord (Lrt a) where
-  (<=) l r = r `subgraph` l
 
 instance Functor Lrt where
   fmap f Nil = Nil
